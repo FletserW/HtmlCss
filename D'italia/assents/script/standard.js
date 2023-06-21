e508f06ddabf680b60a2e36794edcb4bcc6d9fea
FletserW <130801024+FletserW@users.noreply.github.com>
2023-06-21 13:17:38 -0300

    create d'italia

---
+++ D'italia/assents/script/standard.js
@@ -0,0 +1,16 @@
+function menuShow() {
+  let menuMobile = document.querySelector('.mobile-menu');
+  let icon = document.querySelector('.icon');
+
+  if (menuMobile.classList.contains('open')) {
+    menuMobile.classList.remove('open');
+    icon.src = "../img/menu-toggle-icon.png";
+  } else {
+    menuMobile.classList.add('open');
+    icon.src = "../img/Cross-Icon.png";
+  }
+}
+
+function instagram() {
+  window.alert('Me desculpe, link quebrado no momento');
+}
