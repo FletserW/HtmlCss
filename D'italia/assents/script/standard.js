ac537bfaa28cb6525ae05828f5e56a6ca36daca5
FletserW <130801024+FletserW@users.noreply.github.com>
2023-06-23 22:56:16 -0300

    header corrigido

--- D'italia/assents/script/standard.js
+++ D'italia/assents/script/standard.js
@@ -4,13 +4,14 @@ function menuShow() {
 
   if (menuMobile.classList.contains('open')) {
     menuMobile.classList.remove('open');
-    icon.src = "../img/menu-toggle-icon.png";
+    icon.src = "..\img\menu-toggle-icon.png";
   } else {
     menuMobile.classList.add('open');
-    icon.src = "../img/Cross-Icon.png";
+    icon.src = "\img\Cross-Icon.png";
   }
 }
 
 function instagram() {
   window.alert('Me desculpe, link quebrado no momento');
 }
+
