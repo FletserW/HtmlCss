7f285ad55b6254ab38d7771ed2000cee5de1daa1
FletserW <130801024+FletserW@users.noreply.github.com>
2023-06-24 01:18:12 -0300

    wellcome

--- D'italia/assents/script/standard.js
+++ D'italia/assents/script/standard.js
@@ -11,6 +11,21 @@ function menuShow() {
   }
 }
 
+window.addEventListener('load', function() { //Execulta a função ao iniciar o site
+  var overlay = document.getElementById('overlay'); //Seleciona os itens por meio do id
+  var popup = document.getElementById('popup');
+  var closeBtn = document.getElementById('close-btn');
+  //Torna os itens visiveis
+  overlay.style.display = 'block'; //Mudando o estilo da overlay, alternado o tipo de display
+  popup.style.display = 'block';
+  //Desativa a visualização do itens
+  closeBtn.addEventListener('click', function() {
+      overlay.style.display = 'none';
+      popup.style.display = 'none';
+  });
+});
+
+
 function instagram() {
   window.alert('Me desculpe, link quebrado no momento');
 }
